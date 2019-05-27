70e55f7832002abbb687164d2a706f5f78f6a16c
Spike197701 <50291954+Spike197701@users.noreply.github.com>
2019-05-27 16:22:18 +0100

    Add files via upload

---
+++ ZipIt/ZipIt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace ZipIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new file
+            string path = @"C:\Users\Spike\Desktop\Programming Learning\C#Programs\C#Program\ZipIt\Test Data\CombinedData.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File all Ready Exists: " + path);
+            }
+            else 
+            {
+                FileStream newFile = File.Create(path);
+                Console.WriteLine("File Created: " + path);
+                
+            }
+            Console.ReadKey();
+
+            //copy data from exixting file to newly created file
+            
+            File.Copy(@"C:\Users\Spike\Desktop\Programming Learning\C#Programs\C#Program\ZipIt\Test Data\Test Data 1.txt", @"C:\Users\Spike\Desktop\Programming Learning\C#Programs\C#Program\ZipIt\Test Data\CombinedData.txt");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Spike\Desktop\Programming Learning\C#Programs\C#Program\ZipIt\Test Data\Test Data 1.txt"));
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Spike\Desktop\Programming Learning\C#Programs\C#Program\ZipIt\Test Data\CombinedData.txt"));
+            Console.Read();
+            
+            
+            
+        }
+    }
+}
