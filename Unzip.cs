e0d93e6b28749ae7ee5321b4e5851ab34279cb00
Spike197701 <50291954+Spike197701@users.noreply.github.com>
2019-09-22 11:26:25 +0100

    Add files via upload

---
+++ Unzip.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ZipIt
+{
+    class Unzip
+    {
+
+        //Unzip Function
+        //Copy data from newly created file out into seperate files.
+        public static void unzipFiles()
+        {
+            string fileUnzipLocation;
+            string fileExport;
+            Console.WriteLine("Please enter the path and filename of the file you want to unzip: ");
+            fileExport = Console.ReadLine();
+            Console.WriteLine("Please enter the path where you want to unzip the files to: ");
+            fileUnzipLocation = Console.ReadLine();
+
+            string fileOutput = fileUnzipLocation;
+
+
+            if (File.Exists(fileExport))
+            {
+
+                //read the file and get back the filename to create the new file to copy data to.
+                BinaryReader fileReader = new BinaryReader(new FileStream(fileExport, FileMode.Open));
+
+                int fileNameLength = fileReader.ReadInt32();
+                byte[] buffer = new byte[fileNameLength];
+                int bytesRead = fileReader.Read(buffer, 0 , buffer.Length);
+                string bytesConverted = Encoding.UTF8.GetString(buffer, 0, buffer.Length); 
+                string fileName = bytesConverted;
+                Console.WriteLine("Filename is :" + fileName);
+                int fileLength = fileReader.ReadInt32();
+                byte[] bufferLength = new byte[fileLength];
+                fileReader.Read();
+
+                try
+                {
+                    StreamWriter fileWriter = new StreamWriter(fileOutput + fileName);
+                    fileWriter.Write(buffer);
+                }
+
+                finally 
+                {
+                    StreamWriter fileWriter = new StreamWriter(fileOutput + fileName);
+                    fileWriter.Close();
+                }
+
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+        }
+    }
+}
---
+++ Zip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ZipIt
+{
+     class Zip
+    {
+         //Zip Function
+            //copy data to newly created file
+         public static void zipFile(string importPath){
+
+             //string addFile = "yes";
+             string content = null;
+             string filePathInfo = null;
+             string fileName = null;
+             int fileNameLength = 0;
+             long size = 0;
+
+
+
+                Console.WriteLine("Please enter path of file to zip: ");
+                content = Console.ReadLine();
+                filePathInfo = content;
+                FileInfo data = new FileInfo(filePathInfo);
+                fileName = Path.GetFileName(filePathInfo);
+                fileNameLength = fileName.Length;
+                size = data.Length;
+                try
+                {
+                    BinaryWriter writer = new BinaryWriter(new FileStream(importPath, FileMode.Open));
+
+                    writer.Write(fileNameLength);
+                    writer.Write(fileName);
+                    writer.Write(size);
+                    writer.Write(content);
+                    writer.Close();
+                }
+                finally
+                {
+                    BinaryWriter writer = new BinaryWriter(new FileStream(importPath, FileMode.Open));
+                    writer.Close();
+                }
+                      
+               
+            
+            Console.WriteLine("Files copied successfully");
+
+            Console.Read();
+            Console.ReadKey();
+
+            
+         }
+
+         
+    }
+}
